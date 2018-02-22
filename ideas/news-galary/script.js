67bdf2ad226ad13a4281238488332bde08b79003
Unknown <glebaz666@gmail.com>
2018-02-22 17:16:34 +0200

    new hover effects

---
+++ ideas/news-galary/script.js
@@ -0,0 +1,9 @@
+function down()
+{
+  var a = document.getElementById('invisible');
+  if ( a.style.display == 'none' )
+    a.style.display = 'block'
+  else
+    if ( a.style.display == 'block' )
+    a.style.display = 'none';
+};
